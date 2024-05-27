06276c5743dbcedcd9c822f0fc4edbc61304bec8
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2024-05-27 16:09:02 +0300

    added admin_handlers.py

---
+++ handlers/admin_handlers.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def process_cmd_start_admin(message: Message, bot: Bot) -> None:
+    print(type(bot))
+    print(bot)
+    
+    await message.answer(text='Bot is ready, admin')
---
+++ main.py
@@ -0,0 +1,6 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from handlers.admin_handlers import process_cmd_start_admin
