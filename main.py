39053cae2e9af4f4d2f1d14534daa5e9923d458a
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2024-05-27 23:44:24 +0300

    minor change

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from dialogs.user_dialogs import user_start_dialog
 logger = logging.getLogger(__name__)
 
 
+# info
 async def main() -> None:
     logging.basicConfig(
         level=logging.INFO,
