07d7ed715602f2964f1f0b087bccecbfca01a59c
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-11-13 20:11:31 +0300

    Update commands list for main menu button

--- bot/handlers/main_bot_menu/commands_list.py
+++ bot/handlers/main_bot_menu/commands_list.py
@@ -4,5 +4,8 @@ from aiogram.types import BotCommand
 # list of commands for bot`s main menu button
 main_menu_commands = [
     #TODO: remove /start command from this list
-    BotCommand(command="/start", description="Запуск бота")
+    BotCommand(command="/start", description="Запуск бота"),
+    BotCommand(command="/weight", description="Записать вес"),
+    BotCommand(command="/measure", description="Записать объемы"),
+    BotCommand(command="/cancel", description="Отмена"),
 ]
