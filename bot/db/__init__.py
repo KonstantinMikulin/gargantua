a1e3f3a57ba07ee30ffdfd6ac98037019ee853a8
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-11-13 14:48:26 +0300

    Add models for db

--- bot/db/__init__.py
+++ bot/db/__init__.py
@@ -1,3 +1,16 @@
 from .base import Base
+from .models import User, Weight, MeasureChest, MeasureWaist, MeasureHips
+from .requests import add_weight, add_chest, add_waist, add_hips
 
-__all__ = ["Base"]
+__all__ = [
+    "Base",
+    "add_weight",
+    "add_chest",
+    "add_waist",
+    "add_hips",
+    "User",
+    "Weight",
+    "MeasureChest",
+    "MeasureWaist",
+    "MeasureHips"
+]
---
+++ bot/db/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .user_weights import Weight
+from .user_measure_chest import MeasureChest
+from .user_measure_waist import MeasureWaist
+from .user_measure_hips import MeasureHips
+
+__all__ = ["User", "Weight", "MeasureChest", "MeasureHips", "MeasureWaist"]
