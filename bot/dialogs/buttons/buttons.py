9b192b34230d68e76a81927f0f5ce9a5230cfaf9
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-12-02 16:48:26 +0300

    Refactor CANCEL_START_BUTOON

---
+++ bot/dialogs/buttons/buttons.py
@@ -0,0 +1,14 @@
+from aiogram_dialog import StartMode, ShowMode
+from aiogram_dialog.widgets.text import Const
+from aiogram_dialog.widgets.kbd import Start
+
+from bot.dialogs import MainMenuSG
+
+# buttton to switch to main_menu state (call it "cancel")
+CANCEL_START_BUTTON = Start(
+    text=Const("Отмена"),
+    id="cancel_switch_to_main",
+    state=MainMenuSG.main_state,
+    show_mode=ShowMode.DELETE_AND_SEND,
+    mode=StartMode.RESET_STACK,
+)
--- bot/dialogs/weight_dialog.py
+++ bot/dialogs/weight_dialog.py
@@ -1,27 +1,18 @@
-from aiogram_dialog import Dialog, Window, StartMode, ShowMode
+from aiogram_dialog import Dialog, Window
 from aiogram_dialog.widgets.text import Const, Format
 from aiogram_dialog.widgets.input import TextInput
-from aiogram_dialog.widgets.kbd import Button, Row, Start
+from aiogram_dialog.widgets.kbd import Button, Row
 
 # TODO: refactor imports
 from bot.dialogs.getters import weight_getter, weight_delta_getter
 from bot.dialogs import AddWeightSG
-from bot.dialogs.states import MainMenuSG
+from bot.dialogs.buttons import CANCEL_START_BUTTON
 from bot.dialogs.aiogram_dialog_handlers import (
     validate_weight,
     weight_correct_handler,
     weight_error_handler,
     weight_approved,
     change_weight
-)
-
-# buttton to switch to main_menu state (call it "cancel")
-_CANCEL_BUTTON = Start(
-    text=Const("Отмена"),
-    id="cancel_switch_to_main",
-    state=MainMenuSG.main_state,
-    show_mode=ShowMode.DELETE_AND_SEND,
-    mode=StartMode.RESET_STACK,
 )
 
 add_weight_dialog = Dialog(
@@ -33,7 +24,7 @@ add_weight_dialog = Dialog(
             on_success=weight_correct_handler,  # type: ignore
             on_error=weight_error_handler,  # type: ignore
         ),
-        _CANCEL_BUTTON,
+        CANCEL_START_BUTTON,
         state=AddWeightSG.add_weight,
     ),
     Window(
@@ -51,7 +42,7 @@ add_weight_dialog = Dialog(
                 on_click=change_weight,
             ),
         ),
-        _CANCEL_BUTTON,
+        CANCEL_START_BUTTON,
         state=AddWeightSG.weight_done,
         getter=weight_getter,
     ),
@@ -61,7 +52,7 @@ add_weight_dialog = Dialog(
             text="С прошлого взвешивания вы {gain_loose}\n<b>{weight_delta}</b> кг",
             when="is_delta",
         ),
-        _CANCEL_BUTTON,
+        CANCEL_START_BUTTON,
         state=AddWeightSG.weight_progress,
         getter=weight_delta_getter
     ),
