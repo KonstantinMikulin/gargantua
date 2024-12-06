2411b2be0e7086443612789f778c2456ec64487d
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-12-06 21:38:34 +0300

    Add handler to get all records

--- bot/dialogs/aiogram_dialog_handlers/get_measure_handlers.py
+++ bot/dialogs/aiogram_dialog_handlers/get_measure_handlers.py
@@ -3,10 +3,10 @@ from aiogram.types import CallbackQuery
 from aiogram_dialog import DialogManager, ShowMode
 from aiogram_dialog.widgets.kbd import Button
 
-from bot.dialogs.states import GetLastRecordsSG
+from bot.dialogs.states import GetLastRecordsSG, GetAllRecordsSG
 
 
-
+# get one last measurment record
 async def get_last_measurment(
     callback: CallbackQuery,
     button: Button,
@@ -20,3 +20,19 @@ async def get_last_measurment(
         await dialog_manager.switch_to(state=GetLastRecordsSG.get_hips, show_mode=ShowMode.EDIT)
     elif button.widget_id == "get_last_weight":
         await dialog_manager.switch_to(state=GetLastRecordsSG.get_weight, show_mode=ShowMode.EDIT)
+
+
+# get all measurments records
+async def get_all_measurments(
+    callback: CallbackQuery,
+    button: Button,
+    dialog_manager: DialogManager
+):
+    if button.widget_id == "get_all_chest":
+        await dialog_manager.switch_to(state=GetAllRecordsSG.get_all_chest, show_mode=ShowMode.EDIT)
+    elif button.widget_id == "get_all_waist":
+        await dialog_manager.switch_to(state=GetAllRecordsSG.get_all_waist, show_mode=ShowMode.EDIT)
+    elif button.widget_id == "get_all_hips":
+        await dialog_manager.switch_to(state=GetAllRecordsSG.get_all_hips, show_mode=ShowMode.EDIT)
+    elif button.widget_id == "get_all_weights":
+        await dialog_manager.switch_to(state=GetAllRecordsSG.get_all_weights, show_mode=ShowMode.EDIT)
